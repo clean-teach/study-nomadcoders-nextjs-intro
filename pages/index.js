ab763fb854d95837a5268b0bc37fb331b48f0a93
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-02-22 05:57:37 +0900

    #1.3 Routing
    - 목표 :
        - 네비게이션 컴포넌트를 만들어 본다.
    - 구현 :
        - `<Link>` :
            - `<a>` 태그 역할을 제공하는 `Next.js` 의 컴포넌트
            - 클라이언트 사이드 렌더링에서 새로고침 없이 라우팅을 하기 위하여 사용
            - `href` 속성을 제외한 `className`, `style` 등의 속성은 Link 컴포넌트에 직접 넘길 수 없다.
                - 때문에, `href` 속성을 제외한 `className`, `style` 등 나머지 속성은 `<Link>` 태그 안에 `<a>` 태그를 만들어서 부여한다.
                    - 13버전부터 `<Link>` 태그 내부에 `<a>`태그를 넣을 수 없다.
                        - 그냥 `<Link>` 태그에 기타 속성을 사용 할 수 있다.
                        - 사용하기 위해서는 `<Link>` 태그에 `legacyBehavior` Props 를 사용하여야 한다.
                        - 참고 : [https://nextjs.org/docs/messages/invalid-new-link-with-extra-anchor](https://nextjs.org/docs/messages/invalid-new-link-with-extra-anchor)
            - 사용법 :
    
                ```jsx
                import Link from 'next/link';
    
                export default function ComponentName() {
                	return (
                //		<Link href=''>
                //			<a className='' style={}>Home</a>
                //		</Link>
                		<Link href='' className='' style={}>
                			Home
                		</Link>
                	)
                }
                ```
    
        - `[useRouter()](https://nextjs.org/docs/api-reference/next/router)`
            - Route 관련 정보와 메소드를 제공하는 `Next.js` Hook
            - 사용법 :
    
                ```jsx
                import { useRouter } from "next/router";
    
                export default function ComponentName() {
                	const router = useRouter();
                	...
                }
                ```

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
-  const [couter, setCounter] = useState(0);
-
   return (
     <div>
-      <h1>Hello {couter}</h1>
-      <button onClick={() => setCounter(prev => prev +1)}>+</button>
+      <NavBar />
+      <h1>Hello</h1>
     </div>
   );
 }
