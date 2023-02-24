23fc588e3cca7d1894c7b1b9db385593f383ee1b
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-02-25 00:55:17 +0900

    #2.0 Patterns
    - 목표 :
        - `Next.js` 의 다른 여러 기능을 배우면서 미니 영화 앱을 만들어 본다.
    - **Layout Pattern**
        - 방법 :
            1. `components/Layout.js` 파일을 만든다.
    
                ```jsx
                import NavBar from "./NavBar";
    
                export default function Layout({ children }) {
                  return (
                    <>
                			<Header>
                	      <NavBar />
                			</Header>
                      <div>{children}</div>
                			<Footer />
                    </>
                  );
                }
                ```
    
                - `Layout` 관련 컴포넌트들을 사용한다.
                - `{ children }` Props
                    - `react.js` 에서 제공하는 Props
                    - 컴포넌트의 자식 컴포넌트를 불러 올 수 있다.
            2. `pages/_app.js` 파일에서 `Layout.js` 컴포넌트 사용
    
                ```jsx
                import Layout from "../components/Layout";
                import "../styles/globals.css";
    
                export default function ComponentName({ Component, pageProps }) {
                  return (
                    <Layout>
                      <Component {...pageProps} />
                    </Layout>
                  );
                }
                ```
    
        - **Layout Pattern** 의 장점
            - 용도별로 파일을 세분화 하여 분리.
    
                → `pages/_app.js` 파일이 과도하게 커지는 것을 방지 할 수 있다.
    
                - `pages/_app.js` 에는 레이아웃 관련 코드 외에도 여러가지 global 코드가 들어갈 것이다.
    - `**head**`
        - `Next.js` 가 제공하는 컴포넌트
        - 이러한 패키지를 한번에 제공 받는 것은 `Next.js` 의 큰 장점이다.
            - create-react-app 으로 시작하는 프로젝트의 경우에는 따로 설치했어야 할 패키지들을 기본으로 제공한다.
                - `react-helmet` 등
            - 따로 패키지를 설치한다면, 프로젝트와 별개인 컴포넌트, 오류 등이 생기고 이를 신경 써야 함을 의미 한다.
        - 사용법 :
            1. `components/head.js` 파일 작성
    
                ```jsx
                **import Head from "next/head";**
    
                export default function ComponentName({ title }) {
                  return (
                    **<Head>**
                			<meta>
                      <title>{title} | Next Movies</title>
                			<link>
                			...
                    **</Head>**
                  );
                }
                ```
    
                - 파일 이름과 컴포넌트 이름은 상관 없다.

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import HeadCustomizer from "@/components/HeadCustomizer";
+
 export default function Potato() {
     return (
         <div>
+            <HeadCustomizer title="About" />
             <h1>About</h1>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import HeadCustomizer from "@/components/HeadCustomizer";
+
 export default function Home() {
   return (
     <div>
+      <HeadCustomizer title="Home" />
       <h1 className='active'>Hello</h1>
     </div>
   );
