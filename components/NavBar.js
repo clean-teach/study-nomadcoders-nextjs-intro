9f919ef042e7b87200e4bb0966d4ce43806dd69b
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-02-27 20:04:17 +0900

    #2.1 Fetching Data 등
    - .prettierrc 적용
    
    - Style 수정
    
    - `Next.js` 에서는…
        - public directory에 접근 할 때 그냥 `src`  “`/`” 경로로 바로 접근 하면 된다.
        - `<img>` 태그가 아니라, `[next/image` 컴포넌트](https://nextjs.org/docs/api-reference/next/image)를 사용하여야 한다.
            - `next/link` , `next/head` 와 마찬가지로 `Next.js` 에서 제공
            - 본 강의에서는 `next/image` 컴포넌트를 다루지는 않는다. (자세한 강의를 원하면 ****[[풀스택] 캐럿마켓 클론코딩](https://nomadcoders.co/carrot-market)**** 수강)
    - 이번 강의 + 다음 강의 목표
    
        [The Movie Database (TMDB)](https://www.themoviedb.org/?language=ko)
    
        - TMDB 웹사이트에서 API 데이터를 받아온다.
        - `Next.js` 를 이용해서 API Key를 숨겨본다.
            - API Key 를 숨기지 않고 자바스크립트 API Fetch 에 그대로 사용한다면 누구나 우리의 API Key를 사용 할 수 있기 때문..
    - 구현
        1. API Data fetch
            - `async await function`을 이중으로 사용하여, `fetch` data를 바로 `json()` 형태로 변환
            - 구조분해 문법을 사용해서 응답받은 데이터 객체에서 필요한 key에만 접근 할 수 있다.
        2. State 초기값 (type 설정)
            - 비어 있는 Array `[]` 로 설정하면, 응답 성공 시 `JSON` 데이터는 받아볼 수 있지만, 로딩중이라던지 다른 형태의 결과는 저장 할 수 없다.
        3. Rendering
            - 응답 성공 시, 영화 데이터 렌더링
                - 물음표 `?` 문법을 사용해서 결과과 존재 할 때에만 `.map()` 을 사용하여 렌더링 하게 한다면, `useState()` 의 초기값에 구속되지 않고 Array Date를 사용할 수 있다.
            - 로딩중.. 상태 표시
                - 로딩중.. 상태를 브라우저에서 확인 하고 싶다면, 개발자 도구 - 네트워크 텝에서 인터넷 속도를 "slow 3G” 로 설정 후 확인
                    - [ ]  `npm run dev` 실행에서는 확인 불가.. 원인 파악 필요

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,33 +1,55 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from './NavBar.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+// import styles from './NavBar.module.css';
 
 export default function NavBar() {
-    const router = useRouter();
-    console.log(router);
+  const router = useRouter();
+  //   console.log(router);
 
-    return (
-        <nav>
-            <Link href='/' legacyBehavior>
-                <a className={router.pathname === '/'? 'active' : ''}>Home</a>
-            </Link>
-            <Link href='/about' className={router.pathname === '/about'? 'active' : ''}>
-                About
-            </Link>
-            <style jsx>{`
-                a {
-                    text-decoration: none;
-                }
-                .active {
-                    color: tomato;
-                }
-            `}</style>
-            {/* <Link href='/' legacyBehavior>
+  return (
+    <nav>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/" legacyBehavior>
+          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+      {/* <Link href='/' legacyBehavior>
                 <a className={[styles.link, router.pathname === '/'? styles.active : ''].join(' ')}>Home</a>
             </Link>
             <Link href='/about' className={`${styles.link} ${router.pathname === '/about'? styles.active : ''}`}>
                 About
             </Link> */}
-        </nav>
-    )
+    </nav>
+  );
 }
